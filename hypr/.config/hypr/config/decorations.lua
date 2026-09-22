5808a3f481dc26965241604072560b7793123b66
Seanrei Valdeabella <147302321+Shibarashii@users.noreply.github.com>
2026-09-22 18:58:10 +0800

    adjust blur

--- hypr/.config/hypr/config/decorations.lua
+++ hypr/.config/hypr/config/decorations.lua
@@ -35,12 +35,12 @@ hl.config({
     decoration = {
         dim_special = 0.3,
         rounding = 10,
-        active_opacity = 0.95,
-        inactive_opacity = 0.85,
+        active_opacity = 1,
+        inactive_opacity = 0.95,
         fullscreen_opacity = 1,
         blur = {
             size = 5,
-            passes = 4,
+            passes = 2,
             special = true,
         },
     },
