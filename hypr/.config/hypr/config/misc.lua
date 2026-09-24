8608ea3dc4a770c2b20254c16338632455a121c3
Seanrei Valdeabella <147302321+Shibarashii@users.noreply.github.com>
2026-09-24 16:49:14 +0800

    adjust hyprland default hyprland wallpaper and logo config

--- hypr/.config/hypr/config/misc.lua
+++ hypr/.config/hypr/config/misc.lua
@@ -14,6 +14,8 @@ hl.config({
         enable_swallow = true,
         swallow_regex = "(kitty|ghostty|[Kk]onsole|Alacritty|gnome-terminal|xfce[0-9]?-terminal)",
         vrr = 3,
+        force_default_wallpaper = 0,    -- Set to 0 or 1 to disable the anime mascot wallpapers
+        disable_hyprland_logo   = true, -- If true disables the random hyprland logo / anime girl background. :(
     },
     render = {
         direct_scanout = 2,
